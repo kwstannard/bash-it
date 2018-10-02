85c5e6bf1849494371ac84bc606210f4ee52a655
Kelly Stannard <kwstannard@gmail.com>
2018-10-02 17:01:07 -0400

    Allow completions after bundle exec
    Examples:
    `bundle exec rsp` + TAB => `bundle exec rspec`
    `bundle exec rspec --no` + TAB => `bundle exec rspec --no-color`

--- completion/available/bundler.completion.bash
+++ completion/available/bundler.completion.bash
@@ -8,24 +8,38 @@
 # To use, source this file on bash:
 #   . completion-bundle
 
+alias be='bundle exec'
+
+#! bash
+# bash completion for the `bundle` command.
+#
+# Copyright (c) 2011-2013 Daniel Luz <dev at mernen dot com>.
+# Distributed under the MIT license.
+# http://mernen.com/projects/completion-ruby
+#
+# To use, source this file on bash:
+#   . completion-bundle
+
 __bundle() {
   local cur=$2
   local prev=$3
   local bundle_command
   __bundle_get_command
-  COMPREPLY=()
 
   local options
   if [[ $cur = -* ]]; then
-      options="--no-color --verbose"
-      if [[ -z $bundle_command ]]; then
-          options="$options --version --help"
-      fi
+    options="--no-color --verbose"
+    if [[ -z $bundle_command ]]; then
+      options="$options --version --help"
+    fi
+  elif [[ $bundle_command = "exec" ]]; then
+    _bundle_exec_completions
+    return
   else
-      if [[ -z $bundle_command || $bundle_command = help ]]; then
-          options="help install update package exec config check list show
-                   console open viz init gem"
-      fi
+    if [[ -z $bundle_command || $bundle_command = help ]]; then
+      options="help install update package exec config check list show
+      console open viz init gem"
+    fi
   fi
   COMPREPLY=($(compgen -W "$options" -- "$cur"))
 }
@@ -53,3 +67,10 @@ __bundle_get_command() {
 
 complete -F __bundle -o bashdefault -o default bundle
 # vim: ai ft=sh sw=4 sts=2 et
+
+_bundle_exec_completions()
+{
+  if [[ $COMP_CWORD = 2 ]]; then
+    COMPREPLY=($(compgen -c -- "$2"))
+  fi
+}
